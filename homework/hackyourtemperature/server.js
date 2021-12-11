6dcce7ed769056cd0f3c26c557e65f0924235bf1
Danijel C <danijelc+hyf@gmail.com>
2021-12-11 12:53:53 +0100

    Finished homework week1

---
+++ homework/hackyourtemperature/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.send("hello from backend to frontend!");
+});
+
+app.post("/weather", function (req, res) {
+  const cityName = req.body.cityName;
+  res.json(cityName);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
